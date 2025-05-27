b5a408a2d9a608310360feede5168e30730abc48
NdetecT306 <nstasya777@mail.ru>
2025-05-27 10:18:49 +0700

    Create headWithStrVec.h

---
+++ headWithStrVec.h
@@ -0,0 +1,10 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <stdexcept>
+#include <sstream>
+using namespace std;
+string reverseWord(const string& word);
+string alienTranslate(const string& sentence);
+vector<double> PayInMonth(double summa, int month, double p);
