43fd92618317cb4b645af704ed632d0aa76ba17b
NdetecT306 <nstasya777@mail.ru>
2025-05-27 10:25:05 +0700

    Create Random.cpp

---
+++ Random.cpp
@@ -0,0 +1,13 @@
+#include<random>
+#include<numeric>
+#include <chrono>
+#include "random.h"
+using namespace std;
+int RandomNumbers(int start, int end){
+    int num;
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    ranlux24_base generation(seed);
+    uniform_int_distribution<int> distribution(start, end);
+    num = distribution(generation);
+    return num;
+}
