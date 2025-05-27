11853cc2573c3fa526e9dd2e642de7b307fbd703
NdetecT306 <nstasya777@mail.ru>
2025-05-27 10:40:39 +0700

    Create rating.h

---
+++ rating.h
@@ -0,0 +1,8 @@
+#include <vector>
+#include <set>
+#include <string>
+#include <cctype>
+#include <stdexcept>
+#include <sstream>
+using namespace std;
+int Rating(set<pair<string, double>>& studentsSet);
