9596fe1ef8de694e0a7b77b21ebeaefa75499663
NdetecT306 <nstasya777@mail.ru>
2025-05-27 10:19:29 +0700

    Create Intes.h

---
+++ Intes.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+#include <set>
+#include <string>
+#include <cctype>
+#include <stdexcept>
+#include <sstream>
+using namespace std;
+int ProstoeChislo();
+int String();
+int Credit(double summa, int month);
+int Casino(int numParties);
