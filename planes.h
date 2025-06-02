3d3b2b851edc85c87a572268e09d01e4519c8ee0
NdetecT306 <nstasya777@mail.ru>
2025-06-03 00:01:09 +0700

    Update and rename rating.h to planes.h

---
+++ planes.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <random>
+#include <map>
+using namespace std;
+int plain();
+int UNO();
+int Console();
--- rating.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <set>
-#include <string>
-#include <cctype>
-#include <stdexcept>
-#include <sstream>
-using namespace std;
-int Rating(set<pair<string, double>>& studentsSet);
