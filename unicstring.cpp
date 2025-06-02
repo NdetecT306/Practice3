255577e67d5e2cb2652d04f0ddc80f33d870a27f
NdetecT306 <nstasya777@mail.ru>
2025-06-03 00:02:34 +0700

    Update and rename tasks.h to unicstring.cpp

---
+++ unicstring.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+struct RoundData {
+    char symbol;
+    string myString;
+};
+int uniqueString() {
+    setlocale(LC_ALL, "rus");
+    cout << "Введите количество: ";
+    int N;
+    cin >> N;
+    cin.ignore(); 
+    vector<RoundData> rounds(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> rounds[i].symbol;
+        getline(cin >> ws, rounds[i].myString);
+    }
+    string allstring = "";
+    for (const auto& e : rounds) {
+        string tempString = e.myString; 
+        tempString.erase(remove_if(tempString.begin(), tempString.end(),[&](char c) 
+            { 
+                return c == e.symbol;
+            }),
+            tempString.end());
+        allstring += tempString;
+    }
+    vector<char> charsVector(allstring.begin(), allstring.end());
+    sort(charsVector.begin(), charsVector.end());
+    charsVector.erase(unique(charsVector.begin(), charsVector.end()), charsVector.end());
+    string answer(charsVector.begin(), charsVector.end());
+    cout << answer << endl;
+    return 0;
+}
+ 
+
