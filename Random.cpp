2c26033661bd0a327848d18b748c4a9909234685
NdetecT306 <nstasya777@mail.ru>
2025-06-04 14:40:21 +1100

    Delete Random.cpp

--- Random.cpp
+++
@@ -1,9 +0,0 @@
-#include "random.h"
-int RandomNumbers(int start, int end){
-    int num;
-    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-    ranlux24_base generation(seed);
-    uniform_int_distribution<int> distribution(start, end);
-    num = distribution(generation);
-    return num;
-}
